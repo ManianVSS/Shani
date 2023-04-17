6cde7234781a15c45755fbff2fd7c06bd218e630
aditya1993s <aditya1993s@gmail.com>
2023-04-17 15:31:46 +0530

    permissions fix

--- test_mgmt/api/models.py
+++ test_mgmt/api/models.py
@@ -75,7 +75,6 @@ class OrgModel(BaseModel):
     org_group = models.ForeignKey(OrgGroup, on_delete=models.SET_NULL, blank=True, null=True,
                                   verbose_name='organization group')
 
-    
     def can_read(self, user):
         return (self.org_group is None) or self.org_group.is_owner(user) or self.org_group.is_member(user) or (
                 self.published and self.org_group.is_guest(user))
@@ -87,22 +86,13 @@ class OrgModel(BaseModel):
         return (self.org_group is None) or self.org_group.is_owner(user)
 
     def is_owner(self, user):
-        if self.org_group is None:
-            return False
-        is_owner = (self.org_group is None) or self.org_group.is_owner(user)
-        return is_owner
+        return (self.org_group is None) or self.org_group.is_owner(user)
 
     def is_member(self, user):
-        if self.org_group is None:
-            return False
-        is_member = (self.org_group is None) or self.org_group.is_member(user)
-        return is_member
+        return (self.org_group is None) or self.org_group.is_member(user)
 
     def is_guest(self, user):
-        if self.org_group is None:
-            return False
-        is_guest = (self.org_group is None) or self.org_group.is_guest(user)
-        return is_guest
+        return (self.org_group is None) or self.org_group.is_guest(user)
 
 
 class ReviewStatus(models.TextChoices):
