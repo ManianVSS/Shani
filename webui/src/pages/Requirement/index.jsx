31cebc156e5b0ae0b9274e4a739f823a2f2f7a66
aditya1993s <aditya1993s@gmail.com>
2023-12-11 11:35:46 +0530

    Requirement Ui update

--- webui/src/pages/Requirement/index.jsx
+++ webui/src/pages/Requirement/index.jsx
@@ -1,4 +1,24 @@
 import React from "react";
+// import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import BreadcrumbComponent from "./BreadcrumbComponent";
 import RequirementTable from "./RequirementTable";
 import { AgGridReact } from "ag-grid-react";
@@ -12,15 +32,65 @@ import json from "../../components/TreeComponent/data/json";
 import { Col, Row } from "react-bootstrap";
 import IndividualReqComponent from "./IndividualReqComponent";
 
-const options = [
-  {
-    name: "Enable body scrolling",
-    scroll: true,
-    backdrop: false,
-  },
-];
+const drawerWidth = 500;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    }),
+    position: "relative",
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}));
 
 const Requirement = () => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   const { orggroup, reqcatid } = useParams();
   const navigate = useNavigate();
 
@@ -92,20 +162,42 @@ const Requirement = () => {
         setMData(myData);
       });
   }, []);
-  console.log(indiData);
   return (
     <div>
-      <BreadcrumbComponent />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexFlow: "row wrap",
+        }}
+      >
+        <div>
+          <BreadcrumbComponent />
+        </div>
+        <div>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </div>
+      </div>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
 
-      <Row className="justify-content-md-center">
-        <Col style={{ border: "1px solid black" }}>
+        <Main open={open}>
           <div className="ag-theme-alpine" style={{ height: "150px" }}>
             <AgGridReact
               rowStyle={rowStyle}
-              getRowStyle={getRowStyle}
+              // getRowStyle={getRowStyle}
               rowData={mData}
               columnDefs={columnDefs}
               defaultColDef={defaultColDef}
+              rowSelection="single"
               onRowDoubleClicked={(data) => {
                 if (data.data.type == "category") {
                   window.location =
@@ -125,12 +217,34 @@ const Requirement = () => {
               }}
             ></AgGridReact>
           </div>
-        </Col>
-        <Col style={{ border: "1px solid black" }}>
+
+          {/* <TreeComponent data={json} /> */}
+        </Main>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+            },
+          }}
+          variant="persistent"
+          anchor="right"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
           <IndividualReqComponent data={indiData} />
-        </Col>
-        {/* <TreeComponent data={json} /> */}
-      </Row>
+        </Drawer>
+      </Box>
     </div>
   );
 };
